10a64061c5336d6bfeb37b83b1e88871697ff164
Hazel Rella <hazelrella11@gmail.com>
2023-09-16 15:16:33 -0700

    better tailwind usage

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,12 +1,6 @@
 <p class="mt-4">clearly this was a good use of my money</p>
 <div class="flex flex-row justify-center mb-4 mx-8 text-sm self-stretch">
-    <a class="m-4" href="/about">about</a>
-    <a class="m-4" href="https://github.com/HazelTheWitch/eimiko.gay" target="_blank" rel="noreferrer noopener">source</a>
-    <a class="m-4" href="https://ko-fi.com/hazelthewitch" target="_blank" rel="noreferrer noopener">support me</a>
+    <a class="m-4 underline" href="/about">about</a>
+    <a class="m-4 underline" href="https://github.com/HazelTheWitch/eimiko.gay" target="_blank" rel="noreferrer noopener">source</a>
+    <a class="m-4 underline" href="https://ko-fi.com/hazelthewitch" target="_blank" rel="noreferrer noopener">support me</a>
 </div>
-
-<style>
-    a {
-        @apply underline;
-    }
-</style>
