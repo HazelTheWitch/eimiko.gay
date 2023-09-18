370ec2a8147eb0c4d6416e8aebf8360971ae619b
Hazel Rella <hazelrella11@gmail.com>
2023-09-18 05:35:38 -0700

    added size for image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <h1 class="text-3xl">welcome to <strong>eimiko.gay</strong></h1>
 <p class="text-lg">i bought this domain for $3.17 and damn it im gonna use it!</p>
 <div class="flex flex-col justify-center items-center">
-    <img class="rounded-3xl m-4" src="/eimiko_love.png" alt="a beautiful rendition of her excellency, the almighty narukami ohosho, god of thunder and guuji yae in love">
+    <img class="rounded-3xl m-4" width="512" height="512" src="/eimiko_love.png" alt="a beautiful rendition of her excellency, the almighty narukami ohosho, god of thunder and guuji yae in love">
     <p class="text-xs">this is a fucking mess holy shit</p>
 
     <h2 class="text-2xl mt-5">check out some pages :3</h2>
